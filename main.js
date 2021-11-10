76470402561cf0830c46aa5a77a97679e4c24ab2
camila corbalan <camincorbalan@gmail.com>
2021-11-10 15:51:39 -0300

    empezando con JQuery

--- main.js
+++ main.js
@@ -1,10 +1,67 @@
-// //GUARDO LAS VARIABLES DE LA TAREA
-// let formTask = document.getElementById("form-task");
-// let error = document.getElementById("error");
+class Tarea{
+    constructor (name, priori, desc) {
+        this.name = name
+        this.priori = priori
+        this.desc = desc
+    }
+}
+
+//GUARDO EL ARRAY EN EL STORAGE
+let arraytask = JSON.parse(localStorage.getItem('arrayTask')) || []
+
+//METODO PARA AGREGAR LOS TAREAS A LA LISTA
+const create = (tarea) => {
+    if (this.name.value == ""){
+        error.innerHTML = "Debe ingresar una tarea";
+        return error;
+    } else{
+        arraytask.push(tarea)
+        localStorage.setItem('arrayTask', JSON.stringify(arraytask))
+    }
+
+}
 
 
-// //EVENTOS
-// formTask.addEventListener("submit", guardarTarea);
+// //ACCEDO A LOS ELEMENTOS DEL DOM
+const listaTask = document.getElementById('lista-task')
+const formTask = document.getElementById('form-task')
+const inputName = document.getElementById('input-tarea')
+const inputDesc = document.getElementById('input-desc')
+
+const prioridades = ['Sin prioridad', 'Importante', 'Urgente']
+const selectPriori = document.getElementById('input-priori')
+
+//AGREGO ELEMENTOS DESDE DOM
+const fragment = document.createDocumentFragment();
+for (const prioridad of prioridades){
+    const selecItem = document.createElement('OPTION')
+    selecItem.setAttribute('value', prioridad.toLocaleLowerCase())
+    selecItem.textContent = prioridad
+    fragment.appendChild(selecItem)
+}
+
+selectPriori.appendChild(fragment)
+
+//AGREGO LAS TAREAS
+for (let tarea of arraytask) {
+    $('.panel-card').append(`<div> <h3> ${tarea.name} </h3> ${tarea.desc} </div>`)
+}
+
+//EVENT PARA AGREGAR TAREAS
+formTask.addEventListener('submit', (event) => {
+
+    const name = inputName.value
+    const priori = selectPriori.value
+    const desc = inputDesc.value
+
+    const tarea = new Tarea(name, priori, desc)
+
+    create(tarea);
+    show(tarea);
+
+    event.preventDefault();
+})
+
 
 
 //FUNCION PARA VALIIDAR
@@ -41,100 +98,5 @@
 
 //     e.preventDefault()
 // }
-
-//Funcion mostrar
-// function mostrarTarea() {
-//     let tasks = JSON.parse(localStorage.getItem("tasks"));
-//     let taskView = document.getElementById("lista-task");
-//     taskView.innerHTML = '';
-//     for (let i = 0; i < tasks.length; i++){
-//         let inputTarea = tasks[i].inputTarea;
-//         let inputDesc = tasks[i].inputDesc;
-//         taskView.innerHTML= `<div>
-//                                 <h2>${inputTarea}</h2>
-//                                 <p>${inputDesc}</p>
-//                                 <a class="btn btn-danger"> x</a>
-//                             </div>
-//                             `
-//     }
-// }
-
-
-class Tarea{
-    constructor (name, priori, desc) {
-        this.name = name
-        this.priori = priori
-        this.desc = desc
-    }
-}
-
-//GUARDO EL ARRAY EN EL STORAGE
-let arraytask = JSON.parse(localStorage.getItem('arrayTask')) || []
-
-//METODO PARA AGREGAR LOS TAREAS A LA LISTA
-const create = (tarea) => {
-    if (this.name.value === ""){
-        error.innerHTML = "Debe ingresar una tarea";
-    } else{
-        arraytask.push(tarea)
-        localStorage.setItem('arrayTask', JSON.stringify(arraytask))
-    }
-
-}
-const show = (tarea) => {
-    for (let tarea of arraytask) {
-
-        let itemTask = document.createElement('div')
-    
-        itemTask.innerHTML = `
-                            <strong> ${tarea.name}</strong> <br>
-                            <i> ${tarea.desc}</i> <br>
-                                `
-    
-        listaTask.appendChild(itemTask)
-    }
-
-}
-
-
-// //ACCEDO A LOS ELEMENTOS DEL DOM
-const listaTask = document.getElementById('lista-task')
-const formTask = document.getElementById('form-task')
-const inputName = document.getElementById('input-tarea')
-const inputDesc = document.getElementById('input-desc')
-
-const prioridades = ['Sin prioridad', 'Importante', 'Urgente']
-const selectPriori = document.getElementById('input-priori')
-
-//AGREGO ELEMENTOS DESDE DOM
-const fragment = document.createDocumentFragment();
-for (const prioridad of prioridades){
-    const selecItem = document.createElement('OPTION')
-    selecItem.setAttribute('value', prioridad.toLocaleLowerCase())
-    selecItem.textContent = prioridad
-    fragment.appendChild(selecItem)
-}
-
-selectPriori.appendChild(fragment)
-
-//AGREGO LAS TAREAS
-
-//EVENT PARA AGREGAR TAREAS
-formTask.addEventListener('submit', (event) => {
-
-    const name = inputName.value
-    const priori = selectPriori.value
-    const desc = inputDesc.value
-
-    const tarea = new Tarea(name, priori, desc)
-
-    create(tarea);
-    show(tarea);
-
-    event.preventDefault();
-})
-
-
-
 
 
