5d591aa99ab79542276a0904fbce59e409fd75f5
camila corbalan <camincorbalan@gmail.com>
2021-11-24 00:13:40 -0300

    estilos iniciales

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 class Tarea{
     constructor (id, name, priori, desc) {
-        this.id = id
-        this.name = name
-        this.priori = priori
-        this.desc = desc
+        this.id = id;
+        this.name = name.toLowerCase();
+        this.priori = priori;
+        this.desc = desc;
     }
 }
-
 //GUARDO EL ARRAY EN EL STORAGE
-let arraytask = JSON.parse(localStorage.getItem('arrayTask')) || []
+let arraytask = JSON.parse(localStorage.getItem('arraytask')) || []
 
+const pushear = (tarea) => {
+    arraytask.push(tarea);
+    localStorage.setItem('arraytask', JSON.stringify(arraytask));
+}
+const findOne = (name) => {
+    name = name.toLowerCase();
+    const task = arraytask.find(task => task.name === name);
+    return task
+}
 
 // //ACCEDO A LOS ELEMENTOS DEL DOM
 const inputName = document.getElementById('input-tarea')
 const inputDesc = document.getElementById('input-desc')
+const form = document.getElementById('form-task')
+const add = document.getElementById('add-btn')
+let card = document.getElementById('lista-task')
+let boton = document.getElementById('btn-delete')
 let id = 100
-
 
 //AGREGO LOS SELECT DE PRIORIDADES
 const prioridades = ['Sin prioridad', 'Importante', 'Urgente']
@@ -34,100 +45,43 @@ $('#btn-inicial').click(() => {
     $('#form-task').slideToggle(1000);
 })
 
+const renderTareas = () => {
+    for (let tasks of arraytask) {
+        let cardTarea = document.createElement('div');
+        cardTarea.innerHTML = `<h3>${tasks.name}</h3>
+        <p>${tasks.desc}</p>
+        <input type=image src="/img/delete.png" id="btn-delete" id="${tasks.name}"></input>
+        `
 
-//AGREGO LAS TAREAS
-for (let tarea of arraytask) {
-    id = id + 1;
-    $('.panel-card').append(`<div id=card> <h3> ${tarea.name} </h3> ${tarea.desc}<input type=image src="/img/delete.png" id="btn-delete" data-id=${tarea.id}></input> </div> `)
+        card.appendChild(cardTarea);
+        cardTarea.onclick = () => {
+            eliminar(tasks.name) 
+            document.location.reload()
+        }
+    }
 }
+renderTareas();
 
-//AGREGAR TAREAS
-$('#add-btn').on('click', function(event){
+//aca va el evento del formulacio
+form.addEventListener('submit', (event) => {
     const name = inputName.value
     const priori = selectPriori.value
     const desc = inputDesc.value
 
     const tarea = new Tarea(id, name, priori, desc)
     const nameValue = inputName.value
-
     if (nameValue == ''){
         $('#error').append('Debe ingresar una tarea');
         $('#error').fadeOut(2000);
         event.preventDefault();
     } else{
-        arraytask.push(tarea)
-        localStorage.setItem('arrayTask', JSON.stringify(arraytask))
+        pushear(tarea)
     }
-
-
 })
 
-
-
-$('#btn-delete').on('click', function(event) { 
-    let eliminarTask = event.target;
-    let id= parseInt(eliminarTask.dataset.id);
-    removeTarea(id);
-    // const findTarea = id => {
-    //     const tarea = arraytask.find (tarea => tarea.id === id);
-    //     return tarea
-    // }
-    
-    // const removeTarea = (id) => {
-    //     const tarea = findTarea(id)
-    //     const indice = arraytask.indexOf(tarea);
-    //     arraytask.splice(indice, 1);
-    //     localStorage.setItem('arraytask', JSON.stringify(arraytask))
-    // }
-})
-
-//----------------
-
-    const findTarea = id => {
-        const tarea = arraytask.find(tarea => tarea.id == id);
-        return tarea
-    }
-    const deleteTarea = (id) => {
-        const tarea = findTarea (id);
-        const indice = arraytask.indexOf(tarea);
-        arraytask.splice(indice, 1);
-        localStorage.setItem("arrayTask", JSON.stringify(arraytask));
-        location.reload();
-    }
-
-
-
-//---------
-// function eliminarTarea(e){
-//     if(e.target.classList.contains('eliminar')){
-//         e.target.parentElement.remove()
-//     }
-// }
-
-// let indice = arraytask.findIndex((objeto, indice, arraytask) =>{
-//     return objeto.name == 
-// })
-// let eliminar = document.getElementsByClassName('eliminar')
-// let indice = arraytask.findIndex((objeto, indice, arraytask) =>{
-//          return objeto.name == eliminar;
-//     })
-// console.log(indice)
-
-// const eliminar = document.querySelectorAll('#btn-delete')
-
-//----------------------
-// const eliminar = document.querySelectorAll('#btn-delete')
-// function eliminarTarea(e){
-
-//     let indice = e.target.parentNode.indice
-//     arraytask.splice(id, 1)
-//     localStorage.setItem('arrayTask', JSON.stringify(arraytask))
-//     location.reload()
-// }
-
-// for (let i = 0; i < eliminar.length; i++) {
-//     eliminar[i].addEventListener('click', eliminarTarea)
-// }
-
-
-
+const eliminar = (name) => {
+    const task = findOne(name)
+    const index = arraytask.indexOf (task)
+    arraytask.splice(index, 1)
+    localStorage.setItem('arraytask', JSON.stringify(arraytask))
+}
